649a6c79f2df2c27f8e4e1c6f432f87d2ac7ca0d
batu <ba921@nyu.edu>
2015-12-03 16:07:39 -0500

    Added the new game project

---
+++ UnityGames/RaceTheMoon/Assets/Scripts/movement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour {
+
+    Rigidbody rb;
+
+    public float speed = 10;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (rb.velocity.magnitude < 10 && Input.GetKeyDown(KeyCode.W)) {
+            Debug.Log("press accepted");
+            rb.AddForce(Vector3.forward * speed * 1000);
+        }
+
+        if (Input.GetKey(KeyCode.W)){
+            Debug.Log("press accepted");
+            rb.AddForce(Vector3.forward * speed);
+        }
+        if (Input.GetKey(KeyCode.A)){
+            rb.AddForce(Vector3.left * speed);
+        }
+        if (Input.GetKey(KeyCode.D)){
+            rb.AddForce(Vector3.right * speed);
+        }
+
+    }
+}
